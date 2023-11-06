ef4984d123645365749162906511906ebc9e3d73
Érick Santos <ericksantos@mailfence.com>
2023-11-05 23:05:42 -0300

    feat: installs Bulma and configs it

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script>
+import Header from "./layout/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <template>
+  <Header></Header>
   <div>Hello World</div>
 </template>
