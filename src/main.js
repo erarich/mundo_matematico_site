a493f40993b5266b480a64528b8e9ae71fd75baf
Érick Santos <ericksantos@mailfence.com>
2023-11-06 00:01:26 -0300

    fix: removes axios from main.js file

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import router from "./router";
 // const pinia = createPinia();
 const app = createApp(App);
 // app.use(pinia);
-app.use(router, axios);
+app.use(router);
 app.use(vuetify);
 app.mount("#app");
